ca57892fae19d309b162c6ac50ed0e4ecf6eb6a3
bilol200 <145771155+bilol200@users.noreply.github.com>
2023-10-20 19:25:27 +0600

    Add files via upload

---
+++ 5 hw/main.js
@@ -0,0 +1,39 @@
+// const Array1 = [1,2,3,undefined,null,false,32,46];
+// const Array2 = Array1.map(item => String(item));
+// console.log(Array2);
+
+
+
+
+
+// const block = [
+//   { id: 1, name: 'Кубат' }, 
+//   { id: 2, name: 'Нурик' }, 
+//   { id: 3, name: 'Айка' },
+//   { id: 4, name: 'Канышай'}, 
+//   { id: 5, name: 'Айба' }, 
+//   { id: 6, name: 'Нурбек'}
+// ]
+// const box = block.filter(obj => obj.id % 2 === 0);
+
+// console.log(box);
+
+
+
+
+// const dd = [1, 2, 3, undefined, 'Нурик', null, false, 4, 36, 'Айка'];
+
+// const ss = dd.filter(element => !isNaN(element) && typeof element === 'number');
+
+// console.log(ss);
+
+
+
+
+// const dd = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < dd.length; i++) {
+//   const ss = dd[i] * 2;
+//   console.log(ss);
+// }
+                                                                  
